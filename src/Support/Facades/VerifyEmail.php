0e695274a2bff1a613306c36500b8785cb9ca63d
tjbp <github.com@tjbp.net>
2016-01-11 15:02:24 +0000

    Fixed constants in VerifyEmail facade

--- src/Support/Facades/VerifyEmail.php
+++ src/Support/Facades/VerifyEmail.php
@@ -14,21 +14,21 @@ class VerifyEmail extends Facade
      *
      * @var string
      */
-    const VERIFY_LINK_SENT = 'verify_email.sent';
+    const VERIFY_LINK_SENT = 'verify_emails.sent';
 
     /**
      * Constant representing a successfully verified email address.
      *
      * @var string
      */
-    const EMAIL_VERIFIED = 'verify_email.verified';
+    const EMAIL_VERIFIED = 'verify_emails.verified';
 
     /**
      * Constant representing an invalid token.
      *
      * @var string
      */
-    const INVALID_TOKEN = 'verify_email.token';
+    const INVALID_TOKEN = 'verify_emails.token';
 
     /**
      * Get the registered name of the component.
